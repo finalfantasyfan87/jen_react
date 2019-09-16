80bb40db665f9b11773354f79d26d6ef7cb57aa2
finalfantasyfan87 <finalfantasyguru8705@gmail.com>
2019-09-16 04:19:40 -0500

    trying my hand at React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }from 'react';
+import jen from './pics/jen.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className = "App">
+        <div className = "App-header">
+        <h2> Tada a Spinning Jenny:</h2>
+          <img src ={jen} className="App-logo" alt="jenPic" />
+          
+        </div>
+        <p className = "App-intro"></p>
+             </div>
+    );
+  }
 }
 
 export default App;
